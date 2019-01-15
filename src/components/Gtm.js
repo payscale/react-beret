5636d2d6e68bb70b4ff309f8d03e24fe5ed58a5e
PSSEA\daniellef <dani.fenske@payscale.com>
2019-01-15 15:15:36 -0800

    more linting

--- src/components/Gtm.js
+++ src/components/Gtm.js
@@ -1,5 +1,5 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // Google Tag Manager
 const Gtm = ({ containerId }) => (
--- src/components/WebFont.js
+++ src/components/WebFont.js
@@ -1,28 +1,26 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const WebFont = ({ families }) => {
   if (!families || !families.length) {
     return null;
   }
 
-  const familyStrings = families.map(
-    family => `${family.fontName}:${family.weights.join(",")}`
-  );
+  const familyStrings = families.map(family => `${family.fontName}:${family.weights.join(',')}`);
 
   return (
     <script>
       {`WebFontConfig = {
-  google: { families: ${JSON.stringify(familyStrings)} }
+google: { families: ${JSON.stringify(familyStrings)} }
 };
 (function() {
-  var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-  '://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
-  wf.type = 'text/javascript';
-  wf.async = 'true';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(wf, s);
+var wf = document.createElement('script');
+wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+'://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+wf.type = 'text/javascript';
+wf.async = 'true';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(wf, s);
 })();`}
     </script>
   );
@@ -32,15 +30,13 @@ WebFont.propTypes = {
   families: PropTypes.arrayOf(
     PropTypes.shape({
       fontName: PropTypes.string,
-      weights: PropTypes.arrayOf(
-        PropTypes.oneOf([100, 200, 300, 400, 500, 600, 700, 800, 900])
-      )
+      weights: PropTypes.arrayOf(PropTypes.oneOf([100, 200, 300, 400, 500, 600, 700, 800, 900]))
     })
   )
 };
 
 WebFont.defaultProps = {
-  families: [{ fontName: "Roboto", weights: [300, 400, 700, 900] }]
+  families: [{ fontName: 'Roboto', weights: [300, 400, 700, 900] }]
 };
 
 export default WebFont;
