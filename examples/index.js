3c2c2ddfc3c36992efbcbf985a07db0e3dd03a6c
Jim Skerritt <jims@payscale.com>
2019-02-05 17:28:12 -0800

    remove react-helmet dependency

--- examples/index.js
+++ examples/index.js
@@ -6,13 +6,11 @@ const fs = require('fs');
 const path = require('path');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
-const Helmet = require('react-helmet').default;
 const App = require('./App').default;
 
 console.log('rendering example server side react...');
 
 const content = ReactDOMServer.renderToString(React.createElement(App));
-const helmet = Helmet.renderStatic();
 
 const html = `<!doctype html>
 <html lang="en">
@@ -20,8 +18,6 @@ const html = `<!doctype html>
     <meta http-equiv="x-ua-compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>beret example</title>
-    ${helmet.link.toString()}
-    ${helmet.script.toString()}
   </head>
   <body>
     ${content}
