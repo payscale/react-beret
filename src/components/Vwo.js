5f5c72c549523e52b60cd2f20b449acd7c1e2225
Danielle Fenske <daniellef@payscale.com>
2019-02-20 13:25:52 -0800

    fix crossorigin casing, take out that tag on Ads

--- src/components/Vwo.js
+++ src/components/Vwo.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 // Visual Web Optimizer (VWO) (A/B testing)
 const Vwo = ({ accountId, shouldLoadVwoFuncName }) => (
-  <script crossorigin="anonymous"
+  <script crossOrigin="anonymous"
     dangerouslySetInnerHTML={{
       __html: `
 if (typeof window.${shouldLoadVwoFuncName} === 'function' && ${shouldLoadVwoFuncName}()) {
